7fe1c6f80f80c57e82c0fb6cdd2d5b1e7c822442
Oleh Mordach <omordach@gmail.com>
2019-04-04 12:52:52 +0300

    push

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: '1008601981695'
+});
+
+const messaging = firebase.messaging();
